31cced228185ef7e4a211a9874e480858b444522
Jordi Aguilera Zamora <jordi_duck_0@outlook.com>
2024-12-29 16:25:26 +0100

    modify photos

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
   if (!isMounted) return null; // Renderiza null hasta que esté montado
 
   const handleCopy = () => {
-    const textToCopy = 'jordi-az@outlook.com';
+    const textToCopy = 'jordi_az@outlook.com';
 
     navigator.clipboard
       .writeText(textToCopy)
@@ -200,7 +200,7 @@ export default function Home() {
                   src="/GeoTrackAnd.png"
                   alt="Jordi Aguilera"
                   width={300}
-                  height={124}
+                  height={300}
                 />
               </div>
             </a>
@@ -222,6 +222,14 @@ export default function Home() {
                   Web de monitorio de los activos de Field Service.{' '}
                 </p>
               </div>
+              <div className="flex items-center justify-center">
+                <Image
+                  src="/GeoView.png"
+                  alt="Jordi Aguilera"
+                  width={600}
+                  height={600}
+                />
+              </div>
             </a>
 
             <a
@@ -230,15 +238,6 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="flex items-center justify-center">
-                <Image
-                  className="mb-3"
-                  src="/MiStock.png"
-                  alt="Jordi Aguilera"
-                  width={124}
-                  height={124}
-                />
-              </div>
               <h2 className={`mb-3 text-2xl font-semibold`}>
                 MiStock{' '}
                 <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
@@ -249,6 +248,15 @@ export default function Home() {
                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                   App de Power Apps para gestionar inventarios.
                 </p>
+              </div>
+              <div className="flex items-center justify-center">
+                <Image
+                  className="mb-3"
+                  src="/MiStock.png"
+                  alt="Jordi Aguilera"
+                  width={300}
+                  height={300}
+                />
               </div>
             </a>
           </div>
@@ -569,14 +577,21 @@ export default function Home() {
             >
               <div>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
                   width="48"
                   height="48"
-                  viewBox="0 0 32 32"
-                  preserveAspectRatio="xMidYMid"
-                  fill="#8cc84b"
+                  viewBox="0 0 71 80"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
                 >
-                  <path d="M14.656.427c.8-.453 1.82-.455 2.6 0L29.2 7.16c.747.42 1.247 1.253 1.24 2.114v13.5c.005.897-.544 1.748-1.332 2.16l-11.88 6.702a2.6 2.6 0 0 1-2.639-.073l-3.565-2.06c-.243-.145-.516-.26-.688-.495.152-.204.422-.23.642-.32.496-.158.95-.4 1.406-.656.115-.08.256-.05.366.022l3.04 1.758c.217.125.437-.04.623-.145l11.665-6.583c.144-.07.224-.222.212-.38V9.334c.016-.18-.087-.344-.25-.417L16.19 2.244a.41.41 0 0 0-.465-.001L3.892 8.93c-.16.073-.27.235-.25.415v13.37c-.014.158.07.307.215.375l3.162 1.785c.594.32 1.323.5 1.977.265a1.5 1.5 0 0 0 .971-1.409l.003-13.29c-.014-.197.172-.36.363-.34h1.52c.2-.005.357.207.33.405L12.18 23.88c.001 1.188-.487 2.48-1.586 3.063-1.354.7-3.028.553-4.366-.12l-3.4-1.88c-.8-.4-1.337-1.264-1.332-2.16v-13.5a2.46 2.46 0 0 1 1.282-2.141L14.656.427zM18.1 9.785c1.727-.1 3.576-.066 5.13.785 1.203.652 1.87 2.02 1.892 3.358-.034.18-.222.28-.394.267-.5-.001-1.002.007-1.504-.003-.213.008-.336-.188-.363-.376-.144-.64-.493-1.273-1.095-1.582-.924-.463-1.996-.44-3.004-.43-.736.04-1.527.103-2.15.535-.48.328-.624 1-.453 1.522.16.383.603.506.964.62 2.082.544 4.287.5 6.33 1.207.845.292 1.672.86 1.962 1.745.378 1.186.213 2.604-.63 3.556-.684.784-1.68 1.2-2.675 1.442-1.323.295-2.695.302-4.038.17-1.263-.144-2.577-.476-3.552-1.336-.834-.724-1.24-1.852-1.2-2.94.01-.184.193-.312.37-.297h1.5c.202-.014.35.16.36.35.093.6.322 1.25.854 1.6 1.026.662 2.313.616 3.487.635.973-.043 2.065-.056 2.86-.7.42-.367.543-.98.43-1.508-.123-.446-.6-.653-1-.8-2.055-.65-4.285-.414-6.32-1.15-.826-.292-1.625-.844-1.942-1.693-.443-1.2-.24-2.687.693-3.607.9-.915 2.22-1.268 3.47-1.394z" />
+                  <g clipPath="url(#clip0_337_7891)" fill="#5FA04E">
+                    <path d="M35.625 79.5c-1.081 0-2.09-.288-3.028-.792l-9.59-5.686c-1.442-.792-.721-1.08-.289-1.224 1.947-.648 2.308-.792 4.327-1.944.216-.144.504-.072.72.072l7.356 4.391c.288.144.649.144.865 0l28.77-16.628c.289-.144.433-.431.433-.791V23.714c0-.36-.144-.648-.432-.792L35.986 6.366c-.288-.144-.65-.144-.865 0L6.35 22.922c-.29.144-.434.504-.434.792v33.184c0 .287.145.647.433.791l7.86 4.535c4.254 2.16 6.922-.36 6.922-2.879V26.593c0-.432.36-.864.865-.864h3.678c.432 0 .865.36.865.864v32.752c0 5.687-3.1 8.998-8.509 8.998-1.658 0-2.956 0-6.633-1.8l-7.572-4.319A6.073 6.073 0 0 1 .798 56.97V23.786a6.073 6.073 0 0 1 3.028-5.255l28.77-16.628c1.804-1.008 4.255-1.008 6.058 0l28.77 16.628a6.073 6.073 0 0 1 3.029 5.255V56.97a6.073 6.073 0 0 1-3.029 5.254l-28.77 16.628c-.865.36-1.947.648-3.029.648Z" />
+                    <path d="M44.567 56.682c-12.62 0-15.215-5.759-15.215-10.654 0-.432.36-.864.865-.864h3.75c.433 0 .793.288.793.72.577 3.815 2.235 5.687 9.879 5.687 6.057 0 8.652-1.368 8.652-4.607 0-1.871-.72-3.24-10.167-4.175-7.86-.792-12.762-2.52-12.762-8.782 0-5.83 4.903-9.285 13.123-9.285 9.23 0 13.772 3.167 14.35 10.077 0 .216-.073.432-.217.648-.144.144-.36.288-.577.288h-3.822a.844.844 0 0 1-.793-.648c-.865-3.96-3.1-5.255-9.013-5.255-6.634 0-7.427 2.304-7.427 4.031 0 2.088.937 2.736 9.879 3.887 8.869 1.152 13.05 2.808 13.05 8.998 0 6.335-5.263 9.934-14.348 9.934Z" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_337_7891">
+                      <path fill="#fff" d="M0 .5h71v79H0z" />
+                    </clipPath>
+                  </defs>
                 </svg>
               </div>
               <h2 className="text-1xl font-semibold text-center lg:text-left">
@@ -804,7 +819,7 @@ export default function Home() {
 
             <Link
               href="mailto:jordi_az@outlook.com"
-              className="flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
+              className="relative flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -821,15 +836,19 @@ export default function Home() {
                 <path d="M4.698 4.034l16.302 7.966l-16.302 7.966a.503 .503 0 0 1 -.546 -.124a.555 .555 0 0 1 -.12 -.568l2.468 -7.274l-2.468 -7.274a.555 .555 0 0 1 .12 -.568a.503 .503 0 0 1 .546 -.124z"></path>
                 <path d="M6.5 12h14.5"></path>
               </svg>
-              <span className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center">
-                Enviar email
+
+              <span
+                id="tooltip-email"
+                className="absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-12 px-4 py-2 rounded-xl bg-white text-black w-32 transition-opacity duration-200 ease-out opacity-0 group-hover:opacity-100 text-center"
+              >
+                Enviar Mail
               </span>
             </Link>
 
             <button
               id="button-copy-email"
               onClick={handleCopy}
-              className="flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
+              className="relative flex-shrink-0 px-0 py-0 group rounded-lg grid place-content-center"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -852,12 +871,12 @@ export default function Home() {
               >
                 Copiar email
               </span>
+              {copied && (
+                <div className="absolute top-[-2rem] left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-4 py-2 rounded-lg shadow-md text-center">
+                  ¡Texto copiado!
+                </div>
+              )}
             </button>
-            {copied && (
-              <div className="absolute top-0 align-middle bg-green-500 text-white px-4 py-2 rounded-lg shadow-md">
-                ¡Texto copiado!
-              </div>
-            )}
           </div>
         </div>
       </footer>
